07ad2fdd936d8597bb3c86b1af5f42899d932a83
hyuckkim <monkeyhyuck@gmail.com>
2024-01-21 14:00:15 +0900

    fix: 보드 내부 요소 id 생성 함수 고침

--- types/kanbanboard.ts
+++ types/kanbanboard.ts
@@ -13,9 +13,16 @@ export const newKanbanBoard = (
 KanbanBoard => {
   return names.map(v => {
     return {
-      _id: crypto.getRandomValues(new Uint8Array(16)).toString(),
+      _id: generateId(),
       name: v,
       content: []
     }
   });
+}
+
+function generateId() {
+  var S4 = function() {
+     return (((1+Math.random())*0x10000)|0).toString(16).substring(1);
+  };
+  return (`${S4()}${S4()}${S4()}${S4()}`);
 }
